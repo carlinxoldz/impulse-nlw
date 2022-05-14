1e7c7beea694e5b69e0820c4ee91120f47655632
GeoSales Tecnologia <geosales@Mac-mini-de-GeoSales.local>
2022-05-14 17:17:47 -0300

    3

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -2,6 +2,7 @@ import { CloseButton } from "./CloseButton";
 import bugImage from "../assets/bug.svg";
 import ideaImage from "../assets/idea.svg";
 import thoughtImage from "../assets/thought.svg";
+import { useState } from "react";
 
 const feedbackTypes = {
   BUG: {
@@ -27,7 +28,11 @@ const feedbackTypes = {
   },
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -36,16 +41,25 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button>
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                type="button"
+                onClick={() => setFeedbackType(key as FeedbackType)}
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Hello world</p>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com garra por{" "}
